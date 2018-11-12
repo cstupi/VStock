67c8167a218414cb7c7abc3215426e8d363509ba
clay.stup@gmail.com <clay.stup@gmail.com>
2018-11-11 18:51:42 -0800

    Initial commit

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin  = require('mini-css-extract-plugin')
+
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
+function resolve(dir){
+    return path.join(__dirname, '..',dir)
+}
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        hot: true,
+        watchOptions: {
+            poll: true
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                  MiniCssExtractPlugin.loader,
+                  'css-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.(js|vue)$/,
+                use: 'eslint-loader',
+                enforce: 'pre'
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: resolve('static/img'),
+            to: resolve('dist/static/img'),
+            toType: 'dir'
+        }]),
+        new webpack.HotModuleReplacementPlugin(),
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'main.css'
+          })
+    ]
+}
---
+++ src/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="center-content">
+    <hello-component name="World" />
+  </div>
+</template>
+<script>
+import HelloComponent from './components/HelloComponent.vue'
+
+export default {
+  components: {
+    HelloComponent
+  }
+}
+</script>
